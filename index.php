71bcbfbbc8366cbf35ac374196e350f1950477e7
markelovdn <markelovdn@gmail.com>
2021-07-24 23:44:03 +0300

    сom

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
     ?>
 </head>
 <body onload='stopwatch();'>
-
-
     <div class="stopwatch">
         <input type='text' id='disp' maxlength=12 readonly>
         <button type='button' onclick='ss()' id='butt'>Start/Stop</button>
